dfeec52f77ba3bb3888bd009c5a5ab81f99dac0e
Kevin Grassi <kmgrassi@gmail.com>
2021-09-22 09:34:48 -0400

    kg-bug-removed card action area to allow for selecting address details

--- src/pages/details/ContactDetails.tsx
+++ src/pages/details/ContactDetails.tsx
@@ -64,92 +64,90 @@ export function ContactDetails({ contactList, isLoading }) {
       {isLoading && <StyledSpinner />}
       {!isLoading && contactList && contact && (
         <Card className={classes.root}>
-          <CardActionArea>
-            <Grid container>
-              <Grid item xs={12} sm={6}>
-                <CardMedia
-                  className={classes.image}
-                  image={contact.picture.large}
-                  title="Contemplative Reptile"
-                />
-                <Typography
-                  gutterBottom
-                  variant="h5"
-                  component="h2"
-                  align="center"
-                >
-                  {formatToReadableName(contact)}
-                </Typography>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <CardContent>
-                  <Grid container className={classes.container}>
-                    <Grid item container xs={12}>
-                      <Grid item xs={3}>
-                        <Typography className={classes.infoLabel}>
-                          Gender:
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={7}>
-                        <Typography className={classes.infoValue}>
-                          {contact.gender}
-                        </Typography>
-                      </Grid>
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <CardMedia
+                className={classes.image}
+                image={contact.picture.large}
+                title="Contemplative Reptile"
+              />
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="h2"
+                align="center"
+              >
+                {formatToReadableName(contact)}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <CardContent>
+                <Grid container className={classes.container}>
+                  <Grid item container xs={12}>
+                    <Grid item xs={3}>
+                      <Typography className={classes.infoLabel}>
+                        Gender:
+                      </Typography>
                     </Grid>
-                    <Grid item container xs={12}>
-                      <Grid item xs={3}>
-                        <Typography className={classes.infoLabel}>
-                          Address:
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={7}>
-                        <Typography className={classes.infoValue}>
-                          {formatToReadableAddress(contact)}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                    <Grid item container xs={12}>
-                      <Grid item xs={3}>
-                        <Typography className={classes.infoLabel}>
-                          Phone:
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={7}>
-                        <Typography className={classes.infoValue}>
-                          {contact.phone}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-
-                    <Grid item container xs={12}>
-                      <Grid item xs={3}>
-                        <Typography className={classes.infoLabel}>
-                          Cell:
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={7}>
-                        <Typography className={classes.infoValue}>
-                          {contact.cell}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                    <Grid item container xs={12}>
-                      <Grid item xs={3}>
-                        <Typography className={classes.infoLabel}>
-                          Email:
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={7}>
-                        <Typography className={classes.infoValue}>
-                          {contact.email}
-                        </Typography>
-                      </Grid>
+                    <Grid item xs={7}>
+                      <Typography className={classes.infoValue}>
+                        {contact.gender}
+                      </Typography>
                     </Grid>
                   </Grid>
-                </CardContent>
-              </Grid>
+                  <Grid item container xs={12}>
+                    <Grid item xs={3}>
+                      <Typography className={classes.infoLabel}>
+                        Address:
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={7}>
+                      <Typography className={classes.infoValue}>
+                        {formatToReadableAddress(contact)}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item container xs={12}>
+                    <Grid item xs={3}>
+                      <Typography className={classes.infoLabel}>
+                        Phone:
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={7}>
+                      <Typography className={classes.infoValue}>
+                        {contact.phone}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+
+                  <Grid item container xs={12}>
+                    <Grid item xs={3}>
+                      <Typography className={classes.infoLabel}>
+                        Cell:
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={7}>
+                      <Typography className={classes.infoValue}>
+                        {contact.cell}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item container xs={12}>
+                    <Grid item xs={3}>
+                      <Typography className={classes.infoLabel}>
+                        Email:
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={7}>
+                      <Typography className={classes.infoValue}>
+                        {contact.email}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </CardContent>
             </Grid>
-          </CardActionArea>
+          </Grid>
         </Card>
       )}
     </HomePageLayout>
